290f3d0e077e86b655fd4fc128235575c753ec6b
qyz <yonoel@qq.com>
2019-01-14 09:30:16 +0800

    update ignore

---
+++ socket/src/main/java/com/study/first/third/SocketService.java
@@ -0,0 +1,19 @@
+package com.study.first.third;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 多线程的状态 Socket实现多个客户端向服务器端通信 服务器没有响应回去
+ */
+public class SocketService {
+    public static void main(String[] args) throws Exception{
+        ServerSocket serverSocket = new ServerSocket(5208);
+        System.out.println("server has bootstraped");
+        while (true){
+            Socket socket = serverSocket.accept();
+            System.out.println("有人上线了"+ socket.getInetAddress()+":"+ socket.getPort());
+            new Thread(new ServerThread(socket)).start();
+        }
+    }
+}
---
+++ springdata/src/main/java/com/study/springdata/springdatastudy/config/Config.java
@@ -0,0 +1,8 @@
+package com.study.springdata.springdatastudy.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+
+public class Config {
+}
