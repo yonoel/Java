88702dca20b2c12f4efca69ca045015ebb255d89
qianyizhen <yonoel@icloud.com>
2019-01-17 15:57:07 +0800

    开始第三节

---
+++ Algorithms/src/main/java/com/study/chapter/first/two/Evaluate.java
@@ -0,0 +1,12 @@
+package com.study.chapter.first.two;
+
+import java.util.Stack;
+
+public class Evaluate {
+
+    public static void main(String[] args) {
+        Stack<String> ops = new Stack<>();
+        Stack<Double> vals = new Stack<>();
+
+    }
+}
