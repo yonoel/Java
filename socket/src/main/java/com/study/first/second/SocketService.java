f7cc4dafbd230c0951d97d3fc5914caa7e54300b
qyz <yonoel@qq.com>
2019-04-23 14:46:15 +0800

    gradle的一些

--- socket/src/main/java/com/study/first/second/SocketService.java
+++ socket/src/main/java/com/study/first/second/SocketService.java
@@ -13,7 +13,7 @@ import java.net.Socket;
 public class SocketService {
     //搭建服务器端
     public static void main(String[] args) throws IOException {
-        ServerSocket server=new ServerSocket(5209);
+        ServerSocket server=new ServerSocket(80);
         System.out.println("服务器启动成功");
         Socket socket=server.accept();
         BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
